fa0dc6e097e01f4f396da0a460f1bbe2ec5a07d4
Caio <caio_cgf@hotmail.com>
2023-03-28 21:47:00 -0300

    new org

--- db.py
+++ db.py
@@ -1,3 +1,5 @@
 stores = {}
 
-items = {1: {"name": "chair", "price": 2}, 2: {"name": "table", "price": 20}}
+items = {}
+
+# items = {1: {"name": "chair", "price": 2}, 2: {"name": "table", "price": 20}}
