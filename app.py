87e1bcc389d9cc06373f05225f19ca0a8b7b9069
Caio <caio_cgf@hotmail.com>
2023-04-03 21:57:22 -0300

    added tags

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
+import os
+
 from flask import Flask
 from flask_smorest import Api
 
-from db import db
 import models
-
-import os
-
+from db import db
 from resources.item import blp as ItemBlueprint
 from resources.store import blp as StoreBlueprint
+from resources.tag import blp as TagBlueprint
+
 
 def create_app(db_url=None):
     app = Flask(__name__)
@@ -20,7 +21,9 @@ def create_app(db_url=None):
     app.config[
         "OPENAPI_SWAGGER_UI_URL"
     ] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
-    app.config["SQLALCHEMY_DATABASE_URI"] = db_url or os.getenv("DATABASE_URL", "sqlite:///data.db")
+    app.config["SQLALCHEMY_DATABASE_URI"] = db_url or os.getenv(
+        "DATABASE_URL", "sqlite:///data.db"
+    )
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.init_app(app)
     api = Api(app)
@@ -30,5 +33,6 @@ def create_app(db_url=None):
 
     api.register_blueprint(ItemBlueprint)
     api.register_blueprint(StoreBlueprint)
+    api.register_blueprint(TagBlueprint)
 
     return app
